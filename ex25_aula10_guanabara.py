86665506d1cfcdc557261ead041e3a8fcd7beb11
Dagmar Cirino <dagmar.mv@gmail.com>
2018-05-06 04:22:21 -0300

    correção da aula 09

--- ex25_aula10_guanabara.py
+++
@@ -1,9 +0,0 @@
-print("""
-    Exercício 25 da aula 10 de Python
-    Curso do Guanabara
-    Day 7 Code Python - 06/05/2018
-""")
-
-nome = str(input('Qual o seu nome completo: ')).strip()
-
-print('Seu nome tem Cirino? {}'.format('cirino' in nome.lower()))
