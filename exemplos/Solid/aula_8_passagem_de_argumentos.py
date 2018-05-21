10b547359483172306511f7d7d8d2b724db13b01
Dagmar Cirino <dagmar.mv@gmail.com>
2018-05-21 00:57:23 -0300

    Correção passagem por parâmetros com validação

--- exemplos/Solid/aula_8_passagem_de_argumentos.py
+++ exemplos/Solid/aula_8_passagem_de_argumentos.py
@@ -15,26 +15,29 @@ def soma(n1 = 0, n2 = 0):
 def sub(n1 = 0, n2 = 0):
     return n1 - n2
 
-if len(argumentos[1]) > 0:
-    print('maior que 1')
-elif argumentos[1] == 'help':
-    print('''
-        Ajuda do Cirino:
-            argumentos:
-            soma num1 num2 => retorna a soma de num1 + num2
-            sub  num1 num2 => retorna a subtração de num1 - num2
-    ''')
-elif len(argumentos[3]) > 0:
-    if argumentos[1] == 'soma':
-        calc = soma(float(argumentos[2]), float(argumentos[3]))
-    elif argumentos[1] == 'sub':
-        calc = sub(float(argumentos[2]), float(argumentos[3]))
-    else:
-        calc = 'Algo errado ...'
-    print('Calculando...')
-    sleep(1)
-    print(calc)
+if len(argumentos) > 1 and len(argumentos) <= 4:
+    if (argumentos[1]) == 'help':
+        print('''
+            Ajuda do Cirino:
+                argumentos:
+                soma num1 num2 => retorna a soma de num1 + num2
+                sub  num1 num2 => retorna a subtração de num1 - num2
+        ''')
+
+    elif len(argumentos) <= 3:
+        print('Passe os 3 argumentos, ou help para ajuda')
+
+    elif len(argumentos[3]) > 0:
+        if argumentos[1] == 'soma':
+            calc = soma(float(argumentos[2]), float(argumentos[3]))
+        elif argumentos[1] == 'sub':
+            calc = sub(float(argumentos[2]), float(argumentos[3]))
+        else:
+            calc = 'Algo errado ...'
+        print('Calculando...')
+        sleep(1)
+        print(calc)
 else:
-    print('Passe os 3 argumentos, ou help para ajuda')
+    print('Vixe, que erro loko véio. Passe help no parâmetro para rodar certinho a parada.')
 
 print('FIM')
