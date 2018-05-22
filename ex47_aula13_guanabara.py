aee5b9a197b5d89b9a237c4f061a565c4d97b253
Dagmar Cirino <dagmar.mv@gmail.com>
2018-05-22 01:40:33 -0300

    exercício 47 da aula 13 do guanabara

---
+++ ex47_aula13_guanabara.py
@@ -0,0 +1,9 @@
+print('''
+    Exercício da aula 13 de Python
+    Curso do Guanabara
+    Day 22 Code Python - 21/05/2018
+''')
+
+for n in range(2, 500001, 2):
+	print('.', end=' ')
+	print(n, end=' ')
