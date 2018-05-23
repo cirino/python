c749bee42a471d11d9ac2d4083fd75f0e7549bc7
Dagmar Cirino <dagmar.mv@gmail.com>
2018-05-22 22:39:26 -0300

    exercícios  66 da aula 15 do guanabara

---
+++ CursoGuanabara/ex66_aula15_guanabara.py
@@ -0,0 +1,21 @@
+print('''
+    Exercício 66 da aula 15 de Python
+    Curso do Guanabara
+    Day 23 Code Python - 22/05/2018
+''')
+
+print('Somando em loop')
+print('-=' * 20)
+
+cont = soma = 0
+n = int(input('Digite um número. [999] para parar: '))
+
+while n != 999:
+    soma += n
+    n = int(input('Digite um número. [999] para parar: '))
+    cont += 1
+
+print('-=' * 20)
+print('Você digitou {} números, e a soma deles deu {}.'.format(cont, soma))
+print('FIM')
+print('-=' * 20)
